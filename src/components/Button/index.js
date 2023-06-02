6c6a294c1b6e4b80096b5bbf9279cd0e64185d5e
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-02 16:31:27 -0300

    Delete src directory

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'
-import styles from './Button.module.css'
-
-export default function Button({children, path}) {
-    return (
-        <Link href={path} className={ styles.btn }>{children}</Link>
-    )
-}
