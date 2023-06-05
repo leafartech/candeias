2f461bdb097b995729a4623f25763e2fac8c5368
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-05 13:25:52 -0300

    Add files via upload

---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+import styles from './HomeScreen.module.css'
+import Main from '@/components/Main'
+
+export default function HomeScreen() {
+    return (
+        <div className={ styles['home-screen']}>
+            <Header />
+            <Main />
+        </div>
+    )
+}
