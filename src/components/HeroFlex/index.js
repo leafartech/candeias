d945fdd810752240bdcddf949b7796535eb41759
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-06 13:43:47 -0300

    Add files via upload

---
+++ src/components/HeroFlex/index.js
@@ -0,0 +1,26 @@
+import Button from '../Button'
+import Text from '../Text'
+import styles from './HeroFlex.module.css'
+
+export default function HeroFlex({head, children, main, subtitle, img, arr }) {
+    return (
+        <div className={ styles['hero-flex']}>
+            <div className={ `${head ? styles.head : ''} ${styles.text}` }>
+                {img ?
+                <img className={ styles.img } src="./images/logo.png" alt="Logo Clube Candeias" />
+                : ''}
+                <Text start={true} main={main} >
+                    {arr ?
+                        subtitle.map((txt, index) => (
+                            <p key={index} style={{ marginTop: '18px'}}>{txt}</p>
+                        ))
+                    :
+                    <p style={{ marginTop: '30px'}}>{subtitle}</p>
+                    }
+                </Text>
+                <Button path="/">Quero participar do Clube Candeias</Button>
+            </div>
+            {children}
+        </div>
+    )
+}
