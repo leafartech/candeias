2f843218114370d3726315dddd077ccdd1f52a35
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-14 13:02:00 -0300

    Add files via upload

---
+++ src/screens/ThanksScreen/index.js
@@ -0,0 +1,17 @@
+import Text from '@/components/Text'
+import styles from './ThanksScreen.module.css'
+import Section from '@/components/Section'
+
+export default function ThanksScreen() {
+    return (
+        <div className={ styles['thanks-screen']}>
+            <Section className={ styles.text }>
+                {/* <h1>Muito obrigado</h1> */}
+                <Text main="Muito obrigado!" />
+                <h3>Em breve entraremos em contato!</h3>
+                <img src="./images/logo.png" alt="Logo do Clube Candeias" />
+            </Section>
+
+        </div>
+    )
+}
