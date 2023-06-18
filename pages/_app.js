8e6dd53999624d88c32dcb9a2011acc90f6173ec
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-18 16:53:20 -0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
 import '../src/styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Head>
+          <title>Clube Candeias</title>
+          {/* <meta aria-description='' */}
+          <meta name="desc" content="São mais de 500 mil opções de hospedagem no mundo inteiro. Com o Clube Candeias o mundo é o limite." />
+          <link rel="icon" href="./favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
   }
--- pages/_document.js
+++ pages/_document.js
@@ -34,10 +34,6 @@ export default function Document() {
               `,
             }}
           />
-          <title>Clube Candeias</title>
-          {/* <meta aria-description='' */}
-          <meta name="desc" content="São mais de 500 mil opções de hospedagem no mundo inteiro. Com o Clube Candeias o mundo é o limite." />
-          <link rel="icon" href="./favicon.ico" />
        </Head>
       <body>
         <Main />
